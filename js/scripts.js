a142142e275910622b234652b286c486b62579c4
Carson Canales and David Cuthell <david@>
2017-07-06 14:36:52 -0700

    add alert for 21 year olds.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
   var age = parseInt(prompt("How many times has the Egyptian god Ra let you Pass Go?"));
-  if (age>= 21) {
+
+  if (age > 21) {
     $('#drinks').show();
-  } else{
+  } else if (age === 21)  {
+    alert("Congradulations! You are now an adult (sort of...). Get a tatto, while your black out drunk so you can regret it for years to come. Just remeber beer before liquer never been sicker!")
+    $('#under-21').show();
+  }   else{
     $('#under-21').show();
   }
+
 });
