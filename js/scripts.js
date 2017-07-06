e1983888013f5151b7fee2c01e7674292bd11d42
Carson Canales and David Cuthell <david@>
2017-07-06 14:19:01 -0700

    add wells to paragraphs.

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,7 @@ $(document).ready(function(){
   var age = parseInt(prompt("How many times has the Egyptian god Ra let you Pass Go?"));
   if (age>= 21) {
     $('#drinks').show();
+  } else{
+    $('#under-21').show();
   }
 });
